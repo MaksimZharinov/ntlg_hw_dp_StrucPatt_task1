d78359d5417ffea6c3960f2802342ffdbe902506
Maksim Zharinov <minimum.cold1991@gmail.com>
2024-04-27 01:34:54 +0700

    create IntsCalculator class

---
+++ src/main/java/IntsCalculator.java
@@ -0,0 +1,20 @@
+public class IntsCalculator implements Ints {
+    protected final Calculator target;
+
+    public IntsCalculator() { this.target = new Calculator(); }
+
+    @Override
+    public int sum(int arg0, int arg1) {
+        //считаем через target
+    }
+
+    @Override
+    public int mult(int arg0, int arg1) {
+        //считаем через target
+    }
+
+    @Override
+    public int pow(int a, int b) {
+        //считаем через target
+    }
+}
