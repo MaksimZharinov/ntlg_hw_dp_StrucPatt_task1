7e0b547532f9f2c150091edc552cabfa41ce3d77
Maksim Zharinov <minimum.cold1991@gmail.com>
2024-04-27 01:52:44 +0700

    done this task

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,5 +4,8 @@ public class Main {
         System.out.println(calc.sum(2, 2));
         System.out.println(calc.sum(10, 22));
         System.out.println(calc.pow(2, 10));
+
+        //done this task
+        System.out.println(calc.mult(13, 13));
     }
 }
